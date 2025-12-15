c5ee15abf3958ca960650c5eaff0f5879f891c4c
enzoc2000 <s1121673@studenti.univpm.it>
2025-12-15 16:07:05 +0100

    modifica e correzione launch file my_env_pkg

--- my_environment_pkg/launch/my_environment.launch.py
+++ my_environment_pkg/launch/my_environment.launch.py
@@ -4,8 +4,8 @@
   
   Cosa fa:
   1. Avvia Gazebo caricando un mondo specifico ("my_world.world").
-  2. Lancia il file di lancio del robot Doosan (che avvia il robot e i suoi controller).
-  3. Lancia il file di lancio della "sfera" (che probabilmente spawna la sfera 
+  2. Lancia il file di lancio del robot TM5 900(che avvia il robot e i suoi controller).
+  3. Lancia il file di lancio della "sfera" (che spawna la sfera 
      in Gazebo e avvia il nodo 'marker').
   
   In sintesi, orchestra l'avvio di Gazebo, del robot e di un oggetto
@@ -27,18 +27,18 @@ def generate_launch_description():
     # Trova il percorso della cartella 'share' del pacchetto della sfera.
     my_sphere_files       = get_package_share_directory('my_sphere_pkg')
     # Trova il percorso della cartella 'share' del pacchetto del robot Doosan.
-    my_doosan_robot_files = get_package_share_directory('tm5_900') 
+    my_robot_files = get_package_share_directory('tm5_900') 
 
     # 2. Prepara il lancio del Robot Doosan
     # 'IncludeLaunchDescription' è un'azione che permette di lanciare *un altro* file di lancio.
     # Stiamo includendo il file 'my_doosan_controller.launch.py' dal pacchetto 'tm5_900'.
-    doosan_robot = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(my_doosan_robot_files + '/launch/my_tm5_900_controller.launch.py')
+    tm5_900_robot = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(my_robot_files + '/launch/my_tm5_900_controller.launch.py')
     ) 
     
     # 3. Prepara il lancio della Sfera
     # Allo stesso modo, includiamo il file 'my_sphere.launch.py' dal pacchetto 'my_sphere_pkg'.
-    # Questo (probabilmente) avvia sia lo spawn della sfera in Gazebo sia il nodo del marker.
+    # Questo avvia sia lo spawn della sfera in Gazebo sia il nodo del marker.
     sphere_mark  = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(my_sphere_files + '/launch/my_sphere.launch.py')
     ) 
@@ -75,7 +75,7 @@ def generate_launch_description():
 
     # Aggiunge le tre azioni (processi) che abbiamo definito alla lista di avvio.
     # ROS 2 le avvierà in parallelo.
-    ld.add_action (doosan_robot) # Avvia il robot e i controller
+    ld.add_action (tm5_900_robot) # Avvia il robot e i controller
     ld.add_action (sphere_mark)  # Avvia lo spawn della sfera e il marker
     ld.add_action (gazebo_node)  # Avvia la simulazione Gazebo
 
